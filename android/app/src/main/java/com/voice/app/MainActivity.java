2c28ced94f27299e5ac84cac170c6bdc15d3bc75
catrpgorodroleplay-netizen <catrpgorodroleplay@gmail.com>
2026-06-12 12:53:46 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/voice/app/MainActivity.java
+++ android/app/src/main/java/com/voice/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.voice.app;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Этот кусок кода заставит WebView отдать микрофон твоему сайту
+        bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                // Автоматически разрешаем сайту использовать микрофон
+                request.grant(request.getResources());
+            }
+        });
+    }
+}
