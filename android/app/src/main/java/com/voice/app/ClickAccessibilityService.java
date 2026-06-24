50565cc438ca8526e3f7d346ca0e323354069b5f
catrpgorodroleplay-netizen <catrpgorodroleplay@gmail.com>
2026-06-24 10:14:07 +0300

    Create ClickAccessibilityService.java

---
+++ android/app/src/main/java/com/voice/app/ClickAccessibilityService.java
@@ -0,0 +1,47 @@
+package com.voice.app;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.GestureDescription;
+import android.graphics.Path;
+import android.os.Build;
+import android.view.accessibility.AccessibilityEvent;
+
+import java.util.ArrayList;
+
+public class ClickAccessibilityService extends AccessibilityService {
+
+    private static ClickAccessibilityService instance;
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {}
+
+    @Override
+    public void onInterrupt() {}
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    @Override
+    public void onDestroy() {
+        instance = null;
+        super.onDestroy();
+    }
+
+    public static void performClick(ArrayList<MainActivity.ClickPoint> points) {
+        if (instance == null) return;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) return;
+
+        for (MainActivity.ClickPoint point : points) {
+            Path clickPath = new Path();
+            clickPath.moveTo(point.x, point.y);
+            
+            GestureDescription.Builder gestureBuilder = new GestureDescription.Builder();
+            gestureBuilder.addStroke(new GestureDescription.StrokeDescription(clickPath, 0, 1));
+            
+            instance.dispatchGesture(gestureBuilder.build(), null, null);
+        }
+    }
+}
